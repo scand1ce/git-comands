64e15256e3bee392669d99af72c42b4b31595c9f
Serhii_Tomchuk <dice1@ukr.net>
2020-06-22 23:02:16 +0300

    22.06.2020

--- news/templatetags/news_tags.py
+++ news/templatetags/news_tags.py
@@ -4,7 +4,11 @@ from news.models import Category
 
 register = template.Library()
 
-@register.simple_tag()
+@register.simple_tag(name='get_list_categories')
 def get_categories():
     return Category.objects.all()
 
+@register.inclusion_tag('news/list_categories.html')
+def show_categories():
+    categories = Category.objects.all()
+    return {"categories": categories}
--- news/views.py
+++ news/views.py
@@ -5,22 +5,18 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
-
     context = {
-        'categories': categories,
+
         'news': news,
         'title': 'Список новостей',
 
     }
-
     return render(request, template_name='news/index.html', context=context)
 
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news': news, 'categories': categories, 'category': category,})
+    return render(request, 'news/category.html', {'news': news, 'category': category})
 
 
