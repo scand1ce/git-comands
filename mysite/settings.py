31a93a24d60d2d8c48e947419bcb0218e2973ec9
Serhii_Tomchuk <dice1@ukr.net>
2020-06-28 22:50:00 +0300

    '28.06.2020'

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,5 +114,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mysite/static'),
+
+]
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
