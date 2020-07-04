369f417f5f67a23617398ec5a218faa8652eb2e2
Serhii_Tomchuk <dice1@ukr.net>
2020-07-04 14:31:06 +0300

    'forms_part_3'

--- news/models.py
+++ news/models.py
@@ -9,8 +9,7 @@ class News(models.Model):
     update_at = models.DateTimeField(auto_now=True, verbose_name='Обновление')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Состояние')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True,
-                                 blank=True, verbose_name='Категория')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
 
     def get_absolute_url(self):
         return reverse('view_news', kwargs={"news_id": self.pk})
