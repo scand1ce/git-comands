360429c7b69997a7a22b36bc24a25c2b37d020e4
Serhii_Tomchuk <dice1@ukr.net>
2020-07-14 22:52:22 +0300

    'class_DetailView'

--- news/urls.py
+++ news/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     # path('category/<int:category_id>/', get_category, name='category'),
     path('category/<int:category_id>/', NewsByCategory.as_view(extra_context={'title': 'SOME'}),
          name='category'),
-    path('news/<int:news_id>/', view_news, name='view_news'),
+    # path('news/<int:news_id>/', view_news, name='view_news'),
+    path('news/<int:pk>/', ViewNews.as_view(), name='view_news'),
     path('news/add-news/', add_news, name='add_news')
 
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import News, Category
 from .forms import NewsForm
@@ -34,6 +34,11 @@ class NewsByCategory(ListView):
     def get_queryset(self):
         return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
 
+
+class ViewNews(DetailView):
+    model = News
+    context_object_name = 'news_item'
+    # pk_url_kwarg = 'news_id'
 
 '''def index(request):
     news = News.objects.all()
